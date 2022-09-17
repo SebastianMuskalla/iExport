a6bfeb6fb17b271b62e3ed215dbcffd8124fef75
Sebastian Muskalla <SebastianMuskalla@users.noreply.github.com>
2022-09-18 00:31:22 +0200

    package-info

---
+++ src/main/java/iexport/tasks/fileexport/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2014-2022 Sebastian Muskalla
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Classes for an advanced task that exports iTunes playlists in the form of folders
+ * that contain the music files as actual files.
+ */
+package iexport.tasks.fileexport;
